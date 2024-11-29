bbdf9f4529eb5288c5c0fcb5ab423f68168594d6
Brian Mwadime <brian.mwadime@gmail.com>
2024-11-29 21:05:56 +0300

    Update configuration

---
+++ app/src/debug/java/com/game/awesa/ui/MyAppDebug.kt
@@ -0,0 +1,49 @@
+package com.game.awesa.ui
+
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.os.StrictMode.VmPolicy
+import android.util.Log
+import androidx.media3.common.util.UnstableApi
+import com.game.awesa.utils.SystemVersionUtils
+import dagger.hilt.android.HiltAndroidApp
+
+@UnstableApi
+@HiltAndroidApp
+class MyAppDebug: MyApp() {
+    override fun onCreate() {
+
+        super.onCreate()
+        enableStrictMode()
+    }
+
+    /**
+     * enables "strict mode" for testing
+     */
+    private fun enableStrictMode() {
+        StrictMode.setThreadPolicy(
+            ThreadPolicy.Builder()
+                .detectDiskWrites()
+                .detectNetwork()
+                .penaltyLog()
+                .penaltyFlashScreen()
+                .build()
+        )
+
+        StrictMode.setVmPolicy(
+            VmPolicy.Builder()
+                .detectActivityLeaks()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .detectLeakedRegistrationObjects()
+                .penaltyLog()
+                .apply {
+                    if (SystemVersionUtils.isAtLeastP()) {
+                        detectNonSdkApiUsage()
+                    }
+                }
+                .build()
+        )
+        Log.w("MyAppDebug", "Strict mode enabled")
+    }
+}
---
+++ app/src/release/java/com/game/awesa/ui/MyAppRelease.kt
@@ -0,0 +1,8 @@
+package com.game.awesa.ui
+
+import androidx.media3.common.util.UnstableApi
+import dagger.hilt.android.HiltAndroidApp
+
+@UnstableApi
+@HiltAndroidApp
+class MyAppRelease: MyApp()
