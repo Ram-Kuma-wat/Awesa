bd60a1ebd5e61a93e178ea5dc7c967345ffc84cd
Brian Mwadime <brian.mwadime@gmail.com>
2024-12-09 12:29:53 +0300

    Update OnMatchListener.java

--- awesalibs/src/main/java/com/codersworld/awesalibs/listeners/OnMatchListener.java
+++ awesalibs/src/main/java/com/codersworld/awesalibs/listeners/OnMatchListener.java
@@ -1,10 +1,9 @@
 package com.codersworld.awesalibs.listeners;
 
-import com.codersworld.awesalibs.beans.county.CountyBean;
 import com.codersworld.awesalibs.beans.matches.MatchesBean;
 
 public interface OnMatchListener {
     void onMatchClick(MatchesBean.InfoBean mBeanMatch);
     void onVideoClick(MatchesBean.VideosBean mBeanVideo);
-    void onVideoDelete(int position,MatchesBean.VideosBean mBeanVideo);
 }
+
