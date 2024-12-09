a41c09e203adcb8ff81bd5fea8ee86c0e7dcc417
Brian Mwadime <brian.mwadime@gmail.com>
2024-12-09 12:32:26 +0300

    Add video grouping by half

--- app/src/main/java/com/game/awesa/ui/matches/VideoAdapter.kt
+++ app/src/main/java/com/game/awesa/ui/matches/VideoAdapter.kt
@@ -9,93 +9,178 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
 import com.codersworld.awesalibs.beans.matches.MatchesBean.VideosBean
+import com.codersworld.awesalibs.listeners.OnDeleteVideoListener
 import com.codersworld.awesalibs.listeners.OnMatchListener
 import com.codersworld.awesalibs.utils.CommonMethods
 import com.game.awesa.R
+import com.game.awesa.databinding.ItemHeaderBinding
+import com.game.awesa.databinding.ItemInterviewBinding
 import com.game.awesa.databinding.ItemVideoBinding
 import java.io.File
 
-class VideoAdapter(onActonActionMatchListener: OnMatchListener) :
-    ListAdapter<VideosBean, VideoAdapter.GameHolder>(VideoDiffCallback()) {
+class VideoAdapter(onActonActionMatchListener: OnMatchListener, onDeleteVideoListener: OnDeleteVideoListener) :
+    ListAdapter<VideosBean, VideoAdapter.VideoRecyclerViewHolder>(VideoDiffCallback()) {
     var context: Context? = null
-    var mListener: OnMatchListener? = onActonActionMatchListener
-    var half: String = ""
+    private var onClickListener: OnMatchListener? = onActonActionMatchListener
+    private var onDeleteVideoListener: OnDeleteVideoListener? = onDeleteVideoListener
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameHolder {
-        val binding = ItemVideoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return GameHolder(binding, parent.context)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoRecyclerViewHolder {
+        return when(viewType) {
+            R.layout.item_video -> VideoRecyclerViewHolder.GameHolder(
+                ItemVideoBinding.inflate(LayoutInflater.from(parent.context),
+                    parent, false),
+                onClickListener = onClickListener,
+                onDeleteVideoListener = onDeleteVideoListener,
+                parent.context)
+            R.layout.item_header -> VideoRecyclerViewHolder.HeaderHolder(
+                ItemHeaderBinding.inflate(LayoutInflater.from(parent.context),
+                    parent, false),
+                parent.context)
+            R.layout.item_interview -> VideoRecyclerViewHolder.InterviewHolder(
+                ItemInterviewBinding.inflate(LayoutInflater.from(parent.context),
+                    parent, false),
+                onClickListener = onClickListener,
+                onDeleteVideoListener = onDeleteVideoListener,
+                parent.context)
+            else -> throw IllegalArgumentException("Invalid ViewType Provided")
+        }
     }
 
-    override fun onBindViewHolder(holder: GameHolder, position: Int) {
-        holder.bind(getItem(position))
+    override fun getItemViewType(position: Int): Int {
+        val item = getItem(position)
+
+        return when (item.local_id) {
+            -1, -2, -3 -> R.layout.item_header
+            Int.MAX_VALUE -> R.layout.item_interview
+            else -> R.layout.item_video
+        }
     }
 
-    inner class GameHolder(
-        private val binding: ItemVideoBinding,
-        private val context: Context) : RecyclerView.ViewHolder(binding.root),
-        View.OnClickListener {
-        lateinit var video: VideosBean
+    override fun onBindViewHolder(holder: VideoRecyclerViewHolder, position: Int) {
+        when (holder) {
+            is VideoRecyclerViewHolder.GameHolder -> holder.bind(getItem(position))
+            is VideoRecyclerViewHolder.HeaderHolder -> holder.bind(getItem(position))
+            is VideoRecyclerViewHolder.InterviewHolder -> holder.bind(getItem(position))
+        }
+    }
 
-        init {
-            binding.root.setOnClickListener(this)
-            binding.imgDelete.setOnClickListener(this)
+    sealed class VideoRecyclerViewHolder(binding: ViewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        class HeaderHolder(
+            private val binding: ItemHeaderBinding,
+            private val context: Context) : VideoRecyclerViewHolder(binding) {
+            init {}
+
+            fun bind(video: VideosBean) {
+                binding.header.text = when(video.local_id) {
+                    -1 -> context.getString(R.string.lbl_first_half)
+                    -2 -> context.getString(R.string.lbl_second_half)
+                    -3 -> context.getString(R.string.lbl_interview1)
+                    else -> null
+                }
+            }
         }
 
-        fun bind(video: VideosBean) {
-            this.video = video
-            binding.txtHalf.text = if ((video.half == 1)) {
-                context.getString(R.string.lbl_first_half)
-            } else {
-                context.getString(
-                    R.string.lbl_second_half
-                )
-            }
+        class GameHolder(
+            private val binding: ItemVideoBinding,
+            private val onClickListener: OnMatchListener?,
+            private val onDeleteVideoListener: OnDeleteVideoListener?,
+            private val context: Context) : VideoRecyclerViewHolder(binding) {
+            lateinit var video: VideosBean
 
-            if (half.equals(video.half.toString(), ignoreCase = true)) {
+            init {
                 binding.txtHalf.visibility = View.GONE
-            } else {
-                binding.txtHalf.visibility = View.VISIBLE
+                binding.root.setOnClickListener {
+                    val mBean =  this.video
+                    onClickListener?.onVideoClick(mBean)
+                }
+                binding.imgDelete.setOnClickListener {
+                    val mBean =  this.video
+                    mBean.isDelete = "1"
+                    onDeleteVideoListener?.onActionDelete(bindingAdapterPosition, mBean)
+                }
             }
-            binding.imgDelete.setVisibility(View.VISIBLE)
 
-            half = video.half.toString()
-            CommonMethods.setTextWithHtml(video.title, binding.txtTeam)
-            binding.txtTime.text = video.time
-            try {
-                if (CommonMethods.isValidString(video.local_video)) {
-                    binding.imgThumbnail.setImageBitmap(
-                        CommonMethods.createVideoThumb(
-                            context, Uri.fromFile(
-                                File(video.local_video)
+            fun bind(video: VideosBean) {
+                this.video = video
+                binding.imgDelete.setVisibility(View.VISIBLE)
+
+                CommonMethods.setTextWithHtml(video.title, binding.txtTeam)
+                binding.txtTime.text = video.time
+                try {
+                    if (CommonMethods.isValidString(video.local_video)) {
+                        binding.imgThumbnail.setImageBitmap(
+                            CommonMethods.createVideoThumb(
+                                context, Uri.fromFile(
+                                    File(video.local_video)
+                                )
                             )
                         )
-                    )
-                    binding.imgDelete.setVisibility(View.GONE)
-                    binding.pbLoading.visibility = View.VISIBLE
-                } else {
+                        binding.imgDelete.setVisibility(View.GONE)
+                        binding.pbLoading.visibility = View.VISIBLE
+                    } else {
+                        CommonMethods.loadImage(context, video.thumbnail, binding.imgThumbnail)
+                        binding.imgDelete.setVisibility(View.VISIBLE)
+                        binding.pbLoading.visibility = View.GONE
+                    }
+                } catch (ex: Exception) {
                     CommonMethods.loadImage(context, video.thumbnail, binding.imgThumbnail)
-                    binding.imgDelete.setVisibility(View.VISIBLE)
-                    binding.pbLoading.visibility = View.GONE
+                    Log.e("VideosAdapter", ex.localizedMessage, ex)
                 }
-            } catch (ex: Exception) {
-                CommonMethods.loadImage(context, video.thumbnail, binding.imgThumbnail)
-                Log.e("VideosAdapter", ex.localizedMessage, ex)
-            }
 
+            }
         }
 
-        override fun onClick(view: View) {
-            if (mListener != null) {
-                val mBean = getItem(bindingAdapterPosition)
-                if (view.id == R.id.imgDelete) {
+        class InterviewHolder(
+            private val binding: ItemInterviewBinding,
+            private val onClickListener: OnMatchListener?,
+            private val onDeleteVideoListener: OnDeleteVideoListener?,
+            private val context: Context) : VideoRecyclerViewHolder(binding) {
+            lateinit var video: VideosBean
+
+            init {
+                binding.txtHalf.visibility = View.GONE
+                binding.root.setOnClickListener {
+                    val mBean =  this.video
+                    onClickListener?.onVideoClick(mBean)
+                }
+                binding.imgDelete.setOnClickListener {
+                    val mBean =  this.video
                     mBean.isDelete = "1"
-                    mListener!!.onVideoDelete(bindingAdapterPosition, mBean)
-                } else {
-                    mListener!!.onVideoClick(mBean)
+                    onDeleteVideoListener?.onInterviewDelete(bindingAdapterPosition, mBean)
                 }
             }
+
+            fun bind(video: VideosBean) {
+                this.video = video
+                binding.imgDelete.setVisibility(View.VISIBLE)
+                binding.txtTeam.text = context.getString(R.string.lbl_interview1)
+                try {
+                    if (CommonMethods.isValidString(video.local_video)) {
+                        binding.imgThumbnail.setImageBitmap(
+                            CommonMethods.createVideoThumb(
+                                context, Uri.fromFile(
+                                    File(video.local_video)
+                                )
+                            )
+                        )
+                        binding.imgDelete.setVisibility(View.GONE)
+                        binding.pbLoading.visibility = View.VISIBLE
+                    } else {
+                        CommonMethods.loadImage(context, video.thumbnail, binding.imgThumbnail)
+                        binding.imgDelete.setVisibility(View.VISIBLE)
+                        binding.pbLoading.visibility = View.GONE
+                    }
+                } catch (ex: Exception) {
+                    CommonMethods.loadImage(context, video.thumbnail, binding.imgThumbnail)
+                    Log.e("VideosAdapter", ex.localizedMessage, ex)
+                }
+
+            }
         }
+
     }
 }
 
@@ -105,8 +190,8 @@ class VideoDiffCallback : DiffUtil.ItemCallback<VideosBean?>() {
     }
 
     override fun areContentsTheSame(oldItem: VideosBean, newItem: VideosBean): Boolean {
-        return oldItem.local_id == (newItem.local_id) &&
-                oldItem.half == (newItem.half) &&
+        return oldItem.local_id == newItem.local_id &&
+                oldItem.half == newItem.half &&
                 oldItem.match_id == newItem.match_id &&
                 oldItem.reaction == newItem.reaction &&
                 oldItem.time == newItem.time &&
