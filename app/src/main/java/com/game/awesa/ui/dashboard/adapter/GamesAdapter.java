03536121b99c29ffee92110354bf35b60747e04a
Brian Mwadime <brian.mwadime@gmail.com>
2024-12-21 12:02:46 +0300

    Update GamesAdapter.java

--- app/src/main/java/com/game/awesa/ui/dashboard/adapter/GamesAdapter.java
+++ app/src/main/java/com/game/awesa/ui/dashboard/adapter/GamesAdapter.java
@@ -10,11 +10,13 @@ import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.codersworld.awesalibs.beans.game.GameBean;
+import com.codersworld.awesalibs.beans.matches.MatchesBean;
 import com.codersworld.awesalibs.utils.CommonMethods;
 import com.game.awesa.R;
 import com.game.awesa.databinding.ItemGameBinding;
 import com.game.awesa.ui.county.CountyActivity;
 import com.game.awesa.ui.league.LeagueActivity;
+import com.game.awesa.ui.recorder.CameraActivity;
 import com.game.awesa.ui.teams.TeamsActivity;
 
 import java.util.List;
@@ -53,10 +55,29 @@ public class GamesAdapter extends RecyclerView.Adapter {
         }
 
         public void onClick(View view) {
-            if(list.get(getAdapterPosition()).getCounty()>1) {
+            if(list.get(getBindingAdapterPosition()).getCounty()>1) {
                 context.startActivity(new Intent(context, CountyActivity.class).putExtra("game_category", list.get(getAdapterPosition()).getId() + ""));
-            }else{
-                context.startActivity(new Intent(context, LeagueActivity.class).putExtra("game_category", list.get(getAdapterPosition()).getId() + "").putExtra("county", list.get(getAdapterPosition()).getCounty_id() + ""));
+            } else {
+
+//                Intent intent = new Intent(context, CameraActivity.class);
+//                MatchesBean.InfoBean mMatchBean = new MatchesBean.InfoBean();
+//                mMatchBean.setId(32);
+//                mMatchBean.setCounty_id(1);
+//                mMatchBean.setCounty_title("Nairobi");
+//                mMatchBean.setLeague_id(3);
+//                mMatchBean.setLeague_title("KPL");
+//                mMatchBean.setOpponent_team_id(1);
+//                mMatchBean.setTeam_id(2);
+//                mMatchBean.setTeam1("Manchester");
+//                mMatchBean.setTeam2("Arsenal");
+//                mMatchBean.setUser_id(32);
+//                intent.putExtra(CameraActivity.EXTRA_MATCH_BEAN, mMatchBean);
+//                context.startActivity(intent);
+
+                Intent intent = new Intent(context, LeagueActivity.class);
+                intent.putExtra("game_category", list.get(getAdapterPosition()).getId() + "");
+                intent.putExtra("county", list.get(getAdapterPosition()).getCounty_id() + "");
+                context.startActivity(intent);
             }
         }
     }
